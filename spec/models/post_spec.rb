3986ff93b318f71d4c1d66cb993f6268f6d34ad2
davidlawes <davidlawes@hotmail.co.uk>
2018-09-29 22:21:39 +0100

    chore: tidied up files and folders

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,22 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Post, type: :model do
 
   before do
-    @user = User.create({email: 'dave@hotmail.com', password: '123456'})
-    @post = Post.create({title: 'test', user_id: @user.id})
+    @user = User.create({email: "dave@hotmail.com", password: "123456"})
+    @post = Post.create({title: "test", user_id: @user.id})
   end
 
-  it 'is valid' do
+  it "is valid" do
     expect(@post).to be_valid
   end
 
-  it 'is not valid without a user id' do
+  it "is not valid without a user id" do
     expect(subject).to_not be_valid
   end
 
-  it 'can have attachments' do
-    name = 'IMG_20180730_190140_742.jpg'
+  it "can have attachments" do
+    name = "IMG_20180730_190140_742.jpg"
     path = "#{Rails.root}/public/images/#{name}"
     @post.image.attach(io: File.open(path), filename: name)
     expect(@post.image.attached?).to be true
