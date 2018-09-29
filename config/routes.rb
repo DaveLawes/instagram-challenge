a6d50449d6617d666f464642a928ea10143bc48d
davidlawes <davidlawes@hotmail.co.uk>
2018-09-29 16:03:21 +0100

    feature: added like link to view

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 
   root "pages#home"
-  
+
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 end
