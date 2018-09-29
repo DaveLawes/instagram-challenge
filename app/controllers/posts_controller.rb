f3bbe2e2fab4061ea95f2b9eb46b3ce1352661e3
davidlawes <davidlawes@hotmail.co.uk>
2018-09-29 11:12:50 +0100

    fix: added title column to posts schema

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,20 @@
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(message_params)
+    redirect_to posts_url
+  end
+
+  private
+
+  def message_params
+    params.require(:post).permit(:title, :content, images[])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_074435) do
+ActiveRecord::Schema.define(version: 2018_09_29_095903) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_09_29_074435) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "title"
   end
 
   create_table "users", force: :cascade do |t|
