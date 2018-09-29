2b21e191e773d6fbea73f23dd849b3a299534dd3
davidlawes <davidlawes@hotmail.co.uk>
2018-09-29 21:49:40 +0100

    style: added css to project

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( navbar.css )
+Rails.application.config.assets.precompile += %w( posts_index.css )
+Rails.application.config.assets.precompile += %w( form.css )
+Rails.application.config.assets.precompile += %w( homepage.css )
--- spec/features/user_can_see_their_own_posts_spec.rb
+++ spec/features/user_can_see_their_own_posts_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Own posts', type: :feature do
     sign_up
     seed_test_database
     seed_test_database_with_another_user_and_posts
-    click_link 'Your photos' # user handle for rspec tests
+    click_link 'Your Photos' # user handle for rspec tests
     expect(page).to have_content 'Here are all your photos'
     expect(page).to have_content 'bohinj'
     expect(page).to have_css("img[src*='IMG_20180730_190140_742.jpg']")
@@ -18,8 +18,8 @@ RSpec.feature 'Own posts', type: :feature do
   scenario 'After seeing their own photos a user can get back to posts#index' do
     sign_up
     seed_test_database
-    click_link 'Your photos'
-    click_link 'All photos'
+    click_link 'Your Photos'
+    click_link 'All Photos'
     expect(URI.parse(current_url).path).to eq '/posts'
   end
 
